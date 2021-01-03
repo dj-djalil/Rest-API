c32c6d4f2dc9fc2347d76dbc30f6eefe78f269e3
A.Djalil Louhichi <louhichiabdeldjalil@gmail.com>
2021-01-03 15:33:30 +0100

    add exception handling feature

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
 import com.example.module.User;
+import com.example.module.UserErrorResponse;
+import com.example.module.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +35,21 @@ private List<User> usersList;
 
     @RequestMapping("/user/{id}")
     public User getUser(@PathVariable int id){
+        if(id>= usersList.size() || id<0){
+            throw new UserNotFoundException("User Not Found "+id);
+        }
         return usersList.get(id);
+    }
+    // add exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException(UserNotFoundException userNotFoundException){
+        // creation d'une response
+        UserErrorResponse error = new UserErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(userNotFoundException.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        // return ResponseEntity
+        // jackson convert error to JSON automaticly (error <-> response body)
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 }
